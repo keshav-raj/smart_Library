b74d2a6c96e097e5d002b1134caf9b52456a1b79
keshav-raj <keshavkumark7@gmail.com>
2018-03-01 16:13:26 +0530

    commit

--- client/main/controllers/myProfileCtrl.js
+++ client/main/controllers/myProfileCtrl.js
@@ -2,6 +2,14 @@ angular.module('smartLibrary')
 
   .controller('myProfileCtrl', function($scope, $state, $meteor, $stateParams) {
 
+    $scope.helpers({
+      Users: function(){
+        var userId = Meteor.userId();
+        return  users.findOne({_id:userId});
+   }
+   });
+
+
     $scope.logout = function(){
       $meteor.logout();
       $state.go("firstPage");
