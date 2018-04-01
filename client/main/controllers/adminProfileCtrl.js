4c463b67c0015f6ac7dce0a5b3be47ec1fe75086
keshav-raj <keshavkumark7@gmail.com>
2018-04-02 00:25:13 +0530

    final update

--- client/main/controllers/adminProfileCtrl.js
+++ client/main/controllers/adminProfileCtrl.js
@@ -3,5 +3,11 @@ angular.module('smartLibrary')
   .controller('adminProfileCtrl', function($scope, $state, $meteor, $stateParams) {
 
   $scope.credentials = {};
+  $scope.helpers({
+    Users: function(){
+      var userId = Meteor.userId();
+      return  Meteor.users.findOne({_id:userId});
+ }
+ });
 
 });
